809dcd93db109b586016fd11c3448a2a174234b2
flsdaniela <danny_fr87@hotmail.com>
2020-05-19 21:34:15 -0500

    Cambie Y

--- MyFile.R
+++ MyFile.R
@@ -3,6 +3,6 @@ b <- "Hello"
 x <- rnorm(100, 4, 0.5)
 y <- 3 + 2*x + rnorm(100, 0, 1)
 pdf("grafica.pdf")
-plot(x, y, xlab = "ingreso", ylab = "Felicidad")
+plot(x, y, xlab = "ingreso", ylab = "obesidad")
 abline(lm(y ~ x))
 dev.off()
