5b6f83d652bc359ef6a3a32215376a157ad88177
flsdaniela <danny_fr87@hotmail.com>
2020-05-19 21:27:23 -0500

    First Commit

---
+++ MyFile.R
@@ -0,0 +1,8 @@
+a <- c(2, 3, 4)
+b <- "Hello"
+x <- rnorm(100, 4, 0.5)
+y <- 3 + 2*x + rnorm(100, 0, 1)
+pdf("grafica.pdf")
+plot(x, y, xlab = "ingreso", ylab = "Felicidad")
+abline(lm(y ~ x))
+dev.off()
